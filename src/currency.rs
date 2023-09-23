b06c3e133f971443661dbb3e32a135201627f12c
A.L.I.C.E <20363359+alice39@users.noreply.github.com>
2023-09-23 01:42:19 -0400

    re-work the whole currency

--- src/currency.rs
+++ src/currency.rs
@@ -1,100 +1,124 @@
-#[derive(Copy, Clone, Debug)]
-pub enum Currency {
-	KSN,
-	USD,
-	BRL,
-	CAD,
-	GBP,
-	EUR,
-	BDT,
-	COP,
+use std::borrow::Cow;
+
+macro_rules! generate_currency {
+    ($enum_name:ident { $($elements:ident),* }, $array_name:ident) => {
+		#[derive(Debug, Clone, Copy)]
+        pub enum $enum_name {
+            $($elements),*
+        }
+
+        pub const $array_name: &[$enum_name] = &[
+            $($enum_name::$elements),*
+        ];
+    };
 }
 
-pub static ALL_CURRENCY: [Currency; 8] = [
-	Currency::KSN,
-	Currency::USD,
-	Currency::BRL,
-	Currency::CAD,
-	Currency::GBP,
-	Currency::EUR,
-	Currency::BDT,
-	Currency::COP,
-];
+generate_currency!(
+	Currency {
+		Ksn,
+		Usd,
+		Brl,
+		Cad,
+		Gbp,
+		Eur,
+		Bdt,
+		Cop
+	},
+	ALL_CURRENCIES
+);
 
-pub struct CurrencyInfo {
-	pub code: String,
-	pub prefix: String,
-	pub posfix: String,
-	pub name: String,
-	pub picture: String,
+pub struct CurrencyInfo<'a> {
+	pub code: Cow<'a, str>,
+	pub prefix: Cow<'a, str>,
+	pub posfix: Cow<'a, str>,
+	pub name: Cow<'a, str>,
+	pub picture: Cow<'a, str>,
 	pub subunitexp: i32,
 }
 
-impl CurrencyInfo {
-	pub fn new(currency: &Currency) -> Self {
+impl TryFrom<&str> for Currency {
+	type Error = anyhow::Error;
+
+	fn try_from(value: &str) -> Result<Self, Self::Error> {
+		match &value.to_uppercase()[0..3] {
+			"KSN" => Ok(Self::Ksn),
+			"USD" => Ok(Self::Usd),
+			"BRL" => Ok(Self::Brl),
+			"CAD" => Ok(Self::Cad),
+			"GBP" => Ok(Self::Gbp),
+			"EUR" => Ok(Self::Eur),
+			"BDT" => Ok(Self::Bdt),
+			"COP" => Ok(Self::Cop),
+			_ => anyhow::bail!("No matching currency"),
+		}
+	}
+}
+
+impl<'a> From<Currency> for CurrencyInfo<'a> {
+	fn from(currency: Currency) -> Self {
 		match currency {
-			Currency::KSN => CurrencyInfo {
-				code: String::from("KSN"),
-				prefix: String::from("K$"),
-				posfix: String::from("nepers"),
-				name: String::from("Kid Server Neper"), 
-				picture: String::from("https://media.discordapp.net/attachments/1153482364907962509/1153858790341492766/twitchiconpng1-tanglesheep.png"),
+			Currency::Ksn => CurrencyInfo {
+				code: "KSN".into(),
+				prefix: "K$".into(),
+				posfix: "nepers".into(),
+				name: "Kid Server Neper".into(), 
+				picture: "https://media.discordapp.net/attachments/1153482364907962509/1153858790341492766/twitchiconpng1-tanglesheep.png".into(),
 				subunitexp: -2
 			},
-			Currency::USD => CurrencyInfo {
-				code: String::from("USD"),
-				prefix: String::from("U$"),
-				posfix: String::from("dollars"),
-				name: String::from("American Dollar"),
-				picture : String::from("https://media.discordapp.net/attachments/1153482364907962509/1153858888895041599/555526.png"),
+			Currency::Usd => CurrencyInfo {
+				code: "USD".into(),
+				prefix: "U$".into(),
+				posfix: "dollars".into(),
+				name: "American Dollar".into(),
+				picture : "https://media.discordapp.net/attachments/1153482364907962509/1153858888895041599/555526.png".into(),
 				subunitexp: -2
 			},
-			Currency::BRL => CurrencyInfo {
-				code: String::from("BRL"),
-				prefix: String::from("R$"),
-				posfix: String::from("reals"),
-				name: String::from("Brazilian Real"),
-				picture : String::from("https://media.discordapp.net/attachments/1153482364907962509/1153858933551808605/206597.png"),
+			Currency::Brl => CurrencyInfo {
+				code: "BRL".into(),
+				prefix: "R$".into(),
+				posfix: "reals".into(),
+				name: "Brazilian Real".into(),
+				picture : "https://media.discordapp.net/attachments/1153482364907962509/1153858933551808605/206597.png".into(),
 				subunitexp: -2
 			},
-			Currency::CAD => CurrencyInfo {
-				code: String::from("CAD"),
-				prefix: String::from("C$"),
-				posfix: String::from("dollars"),
-				name: String::from("Canadian Dollar"),
-				picture : String::from("https://cdn.discordapp.com/attachments/1153482364907962509/1153861273793081414/555473.png"),
+			Currency::Cad => CurrencyInfo {
+				code: "CAD".into(),
+				prefix: "C$".into(),
+				posfix: "dollars".into(),
+				name: "Canadian Dollar".into(),
+				picture : "https://cdn.discordapp.com/attachments/1153482364907962509/1153861273793081414/555473.png".into(),
 				subunitexp: -2
 			},
-			Currency::GBP => CurrencyInfo {
-				code: String::from("GBP"),
-				prefix: String::from("£"),
-				posfix: String::from("pounds"),
-				name: String::from("British Pound"),
-				picture : String::from("https://media.discordapp.net/attachments/1153482364907962509/1153861461182001212/555417.png"),
+			Currency::Gbp => CurrencyInfo {
+				code: "GBP".into(),
+				prefix: "£".into(),
+				posfix: "pounds".into(),
+				name: "British Pound".into(),
+				picture : "https://media.discordapp.net/attachments/1153482364907962509/1153861461182001212/555417.png".into(),
 				subunitexp: -2
 			},
-			Currency::EUR => CurrencyInfo {
-				code: String::from("EUR"),
-				prefix: String::from("€"),
-				posfix: String::from("euros"),
-				name: String::from("European Euro"),
-				picture : String::from("https://media.discordapp.net/attachments/1153482364907962509/1153861704141262858/330426.png"),
+			Currency::Eur => CurrencyInfo {
+				code: "EUR".into(),
+				prefix: "€".into(),
+				posfix: "euros".into(),
+				name: "European Euro".into(),
+				picture : "https://media.discordapp.net/attachments/1153482364907962509/1153861704141262858/330426.png".into(),
 				subunitexp: -2
 			},
-			Currency::BDT => CurrencyInfo {
-				code: String::from("BDT"),
-				prefix: String::from("৳"),
-				posfix: String::from("takas"),
-				name: String::from("Bangladeshi Taka"),
-				picture : String::from("https://cdn.discordapp.com/attachments/1153482364907962509/1153862592121548800/5327225.png"),
+			Currency::Bdt => CurrencyInfo {
+				code: "BDT".into(),
+				prefix: "৳".into(),
+				posfix: "takas".into(),
+				name: "Bangladeshi Taka".into(),
+				picture : "https://cdn.discordapp.com/attachments/1153482364907962509/1153862592121548800/5327225.png".into(),
 				subunitexp: -2
 			},
-			Currency::COP => CurrencyInfo {
-				code: String::from("COP"),
-				prefix: String::from("$"),
-				posfix: String::from("pesos"),
-				name: String::from("Colombian Peso"),
-				picture : String::from("https://media.discordapp.net/attachments/1153482364907962509/1154949125960372235/330508.png"),
+			Currency::Cop => CurrencyInfo {
+				code: "COP".into(),
+				prefix: "$".into(),
+				posfix: "pesos".into(),
+				name: "Colombian Peso".into(),
+				picture : "https://media.discordapp.net/attachments/1153482364907962509/1154949125960372235/330508.png".into(),
 				subunitexp: -2
 			},
 		}
